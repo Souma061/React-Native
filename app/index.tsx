7a33f02c4864f9a23e431c235605d2a648539f42
Souma061 <soumabrataghosh57@gmail.com>
2026-03-29 09:28:21 +0530

    feat: add image background and enhance FlatList with header and empty state

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
-import { Button, Pressable,  Switch, Text, TextInput, View } from "react-native";
+import { Button, FlatList, Image, ImageBackground, Pressable, Switch, Text, TextInput, View } from "react-native";
 
+
+
+
+const data = [
+  { id: "1", name: "John" },
+  { id: "2", name: "Jane" },
+  { id: "3", name: "Bob" },
+  { id: "4", name: "Alice" },
+]
 function HomeScreen() {
 
   const [isEnabled, setIsEnabled] = useState(false);
@@ -22,6 +31,27 @@ function HomeScreen() {
           }}>Home Screen {i + 1}</Text>
         })
       } */}
+
+      < Image
+        style={{ width: 50, height: 50, marginTop: 20, marginLeft: 20 }}
+        source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+      />
+      <ImageBackground
+        source={{ uri: 'https://picsum.photos/seed/picsum/400/400' }}
+        imageStyle={{ resizeMode: 'cover' }}
+        style={{
+          width: 200,
+          height: 200,
+          marginTop: 20,
+          marginLeft: 20,
+          overflow: 'hidden'  // important on iOS
+        }}
+      >
+        <Text style={{ color: "white", fontSize: 18, fontWeight: "bold" }}>
+          Image Background
+        </Text>
+      </ImageBackground>
+
       <Pressable onPress={() => {
         console.log("Pressed");
       }}>
@@ -51,6 +81,19 @@ function HomeScreen() {
         thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
         onValueChange={toggleSwitch}
         value={isEnabled}
+      />
+
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Text>{item.name}</Text>
+        )}
+        ListHeaderComponent={<Text style={{ fontSize: 24, fontWeight: "bold", textAlign: "center", marginTop: 20 }}>Names</Text>}
+        ListEmptyComponent={<Text>No names available.</Text>}
+        ItemSeparatorComponent={() => <View style={{ height: 1, backgroundColor: "gray" }} />}
+        onRefresh={() => console.log("Refreshing...")}
+        refreshing={false}
       />
     </View>
 
